12fee7718f199619caf678e1b429c9cb7a54c364
gaba-tope <topaize3460@gmail.com>
2025-02-01 22:29:34 -0500

    fix: cors-induced fetch failure fixed2

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,14 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors')({
-  origin: [
-    'https://localhost:4000',
-    'https://gaba-tope.github.io'
-  ],
-});
+//const cors = require('cors')({
+//  origin: [
+//    'https://localhost:4000',
+//    'https://gaba-tope.github.io'
+//  ],
+//});
 
-//const cors = require('cors')({ origin: 'http://localhost:4000' }); // Replace with your Jekyll site's origin
+const cors = require('cors')({ origin: 'https://gaba-tope.github.io' }); // Replace with your Jekyll site's origin
 
 admin.initializeApp();
 
