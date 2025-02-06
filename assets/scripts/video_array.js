0e1117235ece51f66f72d529fc7c841c8fa455b3
gaba-tope <topaize3460@gmail.com>
2025-02-06 00:10:53 -0500

    feat: replaced with royalty-free musics

--- assets/scripts/video_array.js
+++ assets/scripts/video_array.js
@@ -1,18 +1,5 @@
 export const videos = [
-    { id: 'dH3GSrCmzC8', title: 'Bill Evans - Waltz for Debby' },
-    { id: 'K9D9gXnH6eY', title: 'Duke Jordan - Glad I Met Pat, Take 3' },
-    { id: 'eo9GCpQDFXQ', title: '岩崎琢 - The Message from The Past'},
-    { id: '7MHQfOtaBQU', title: '神前暁 - The Mysterious' },
-    { id: 'MOzZe_yVIcc', title: '남구민 - Reminiscence' },
-    { id: '8EQY2vVu5Is', title: '坂本龍一 - Blu'},
-    { id: 'c9eSHBFV7N0', title: "Henry Mancini - Breakfast at Tiffany's"},
-    { id: '6DhnS0OANhQ', title: "陈宇鹏 - Call It a Day in Liyue"},
-    { id: 'IdB5QQVO8nU', title: "Andrew York - Evening Dance (Botti Guitar Duo 한은·허유림 연주)"},
-    { id: 'TD1IZeuzQt0', title: "임하영 - 천 년의 사랑"},
-    { id: 'hm8y6kzXwEU', title: "David Lanz - Cristofori's Dream"},
-    { id: 'qH6Rribub-c', title: "Seycara - Pastel | Piano & Cello"},
-    { id: 'EpVXH3Vm2wg', title: "Bill Evans Trio - My Foolish Heart"},
-    { id: 'k6Q8vW_mqTE', title: "Bill Evans Trio - On Green Dolphin Street"},
-    { id: 'Vb_JYarIub4', title: "Chet Baker Quartet - No Problem"},
-    { id: 'ySnM0y_VP3M', title: "坂本龍一 - Thousand Knives"}
+    { id: 'k7kQI2hTVeM', title: 'Rachmaninoff - Piano Concerto No.2, Op.18 II. Adagio sostenuto (2nd Movement)' },
+    { id: 'H8f-0_nZrVQ', title: 'Chopin - Ballade No.4 in F minor, Op.52'},
+    { id: 'YOx710drHnw', title: "Rachmaninoff - Prelude No.2 in C Sharp Minor Op.3"}
 ];
