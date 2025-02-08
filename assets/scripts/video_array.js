02f1d1d6613e6101e52a868e372c3bf41522996f
gaba-tope <topaize3460@gmail.com>
2025-02-08 15:29:21 -0500

    feat: new videos added

--- assets/scripts/video_array.js
+++ assets/scripts/video_array.js
@@ -1,5 +1,10 @@
 export const videos = [
     { id: 'k7kQI2hTVeM', title: 'Rachmaninoff - Piano Concerto No.2, Op.18 II. Adagio sostenuto (2nd Movement)' },
     { id: 'H8f-0_nZrVQ', title: 'Chopin - Ballade No.4 in F minor, Op.52'},
-    { id: 'YOx710drHnw', title: "Rachmaninoff - Prelude No.2 in C Sharp Minor Op.3"}
+    { id: 'YOx710drHnw', title: "Rachmaninoff - Prelude No.2 in C Sharp Minor Op.3"},
+    { id: '6RevYn8Mhkw', title: "Erik Satie - Gymnopedie No.3 (Played by Wahneta Meixsell)"},
+    { id: 'UjFFPoPYV-A', title: "Erik Satie - Gymnopedie No.1 (Played by Kevin MacLeod)"},
+    { id: 'Q9KJYxR4SOc', title: "Maurice Ravel - Gaspard de la Nuit, M. 55, III Scarbo (Played by Peter Bradley-Fulgoni)"},
+    { id: '6u2Q_u3xYVo', title: "Maurice Ravel: Gaspard de la Nuit, M. 55, I. Ondine (Played by Peter Bradley-Fulgoni)"},
+    { id: '88GGfYJxT1g', title: "Erik Satie - Gnossienne No.3 (Played by Reinbert de Leeuw)"}
 ];
