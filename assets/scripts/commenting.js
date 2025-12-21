37855bc8e00afc78f26451dedad0b53ad48b73b3
gaba-tope <topaize3460@gmail.com>
2025-12-21 15:01:30 +0900

    feat: changed unlocked comment icon to opened_lock

--- assets/scripts/commenting.js
+++ assets/scripts/commenting.js
@@ -49,7 +49,7 @@ fetch('https://us-central1-like-button-88f77.cloudfunctions.net/getFirebaseConfi
             lockIcon.innerHTML = `
                 <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                 <circle cx="12" cy="16" r="1"></circle>
-                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
             `;
             lockIcon.style.color = 'currentColor';
             secretMessage.style.display = 'none';
